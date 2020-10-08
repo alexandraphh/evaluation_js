b44ded8e06795620f2206677b713e41163dc0274
alexandraphh <alexandrapham03@gmail.com>
2020-10-08 22:41:22 +0200

    feat(progress bar): Exercice 2, create a progress bar

---
+++ ex2/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * drawBar class represents a loading bar
+ * @param {number} nbr is the proportion of the loaded segment of the bar
+ * @param {number} sum is the total value of our drawBar
+ */
+var drawBar = function drawBar(nbr, sum) {
+  this.nbr = nbr;
+  this.sum = sum;
+};
+
+// Create a progress bar in the HTML
+drawBar.prototype.run = function () {
+  var progress = document.createElement("progress");
+  progress.value = this.nbr;
+  progress.max = this.sum;
+  progress.style.fontSize = "60px";
+  document.body.appendChild(progress);
+};
+
+// Test
+var bar = new drawBar(50, 100);
+bar.run();
